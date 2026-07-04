afca5fd9849d4d461d474481c44480093e7222f4
juanesHc <juanest006@gmail.com>
2026-07-04 18:20:48 -0500

    Query for register and retrieve analysis , cache

--- backend/db.py
+++ backend/db.py
@@ -1,6 +1,7 @@
 import psycopg
 from dotenv import load_dotenv
 from utils import getDataFromEnv
+from psycopg.types.json import Jsonb
 
 load_dotenv()
 
@@ -22,4 +23,31 @@ def checkDailyQuotas(conn, ip_hash):
         (count,) = cur.fetchone()   
     conn.commit()                   
     return count
+
+def readAnalysis(conn, ticker):
+    with conn.cursor() as cur:
+        cur.execute( "SELECT analysis FROM analysis " \
+        "WHERE ticker = %s AND generated_at > now() - interval '24 hours'"
+        ,(ticker,)
+        )
+
+        fila = cur.fetchone()
+        if fila is None:
+            return None
+        else:
+            return fila[0]
+        
+def registerAnalysis(conn, ticker, analysis, fundamentals, price=None):
+    with conn.cursor() as cur:
+        cur.execute(
+            "INSERT INTO analysis (ticker, analysis, fundamentals, price_at_analysis) " \
+            "VALUES (%s, %s, %s, %s) ON CONFLICT (ticker) " \
+            "DO UPDATE SET analysis = EXCLUDED.analysis," \
+            " fundamentals = EXCLUDED.fundamentals," \
+            " price_at_analysis = EXCLUDED.price_at_analysis, " \
+            "generated_at = now()",
+            (ticker, Jsonb(analysis), Jsonb(fundamentals), price),
+        )
+    conn.commit()
+
 
